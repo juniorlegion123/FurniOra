14b489a343af9d5f1210378af5a0e4332cb8978d
juniorlegion123 <shubhamprabhu02@gmail.com>
2025-05-12 23:21:17 +0530

    commit

--- customer/includes/db.php
+++ customer/includes/db.php
@@ -1,5 +1,13 @@
 <?php
+  $host = getenv('DB_HOST');
+  $user = getenv('DB_USER');
+  $pass = getenv('DB_PASS');
+  $name = getenv('DB_NAME');
+  $port = getenv('DB_PORT');
 
-  $con=mysqli_connect("localhost","root","","myshop");
+  $con = mysqli_connect($host, $user, $pass, $name, $port);
 
- ?>
+  if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+?>
